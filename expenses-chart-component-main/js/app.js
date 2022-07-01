fbb906d2729359453e8cfb8da63fc76c01307a2d
LiamMcBride <mailmcbride56@gmail.com>
2022-07-01 17:03:29 -0400

    Title dynamically changes based on input json

--- expenses-chart-component-main/js/app.js
+++ expenses-chart-component-main/js/app.js
@@ -1,5 +1,10 @@
 function calculateHeight(amount){
-    return (amount / 100) * 200;
+    return (amount / 10000) * 400;
+}
+
+function setMetaData(data){
+  document.getElementsByClassName("title")[0].innerHTML = data.title;
+  document.getElementsByClassName("total")[0].innerHTML = data.total;
 }
 
 function sizeGraph(data){
@@ -7,12 +12,18 @@ function sizeGraph(data){
     let titles = document.getElementsByClassName("graph-label");
     let values = document.getElementsByClassName("graph-amount");
     let counter = 0;
+    let totalValue = 0;
     for(const day in data){
         graphs[counter].style.height = calculateHeight(data[day]["amount"]) + "px";
         titles[counter].innerHTML = data[day]["day"]
-        values[counter].innerHTML = "$" + data[day]["amount"];
+        values[counter].innerHTML = data[day]["amount"];
+        totalValue += data[day]["amount"];
         counter++;
     }
+
+    document.getElementsByClassName("total-heading")[0].innerHTML = totalValue;
+    console.log(totalValue)
+
 }
 
 function generateGraph(size){
@@ -40,36 +51,81 @@ function generateGraph(size){
   }
 }
 
-let data = [
-    {
-      "day": "mon",
-      "amount": 17.45
-    },
-    {
-      "day": "tue",
-      "amount": 34.91
-    },
-    {
-      "day": "wed",
-      "amount": 52.36
-    },
-    {
-      "day": "thu",
-      "amount": 31.07
-    },
-    {
-      "day": "fri",
-      "amount": 23.39
-    },
-    {
-      "day": "sat",
-      "amount": 43.28
-    },
-    {
-      "day": "sun",
-      "amount": 25.48
-    }
-  ];
+let data = {
+  "meta": {
+    "title": "Calories Eaten",
+    "total": "Total this week",
+  },
+  "data": [
+      {
+        "day": "6/25",
+        "amount": 2914
+      },
+      {
+        "day": "6/26",
+        "amount": 3160
+      },
+      {
+        "day": "6/27",
+        "amount": 3859
+      },
+      {
+        "day": "6/28",
+        "amount": 2760
+      },
+      {
+        "day": "6/29",
+        "amount": 2714.56
+      },
+      {
+        "day": "6/30",
+        "amount": 2770
+      },
+      {
+        "day": "7/01",
+        "amount": 1960
+      }
+    ]
+  };
+// let data = {
+//   "meta": {
+//     "title": "Calories Eaten",
+//     "total": "Total this week",
+//   },
+//   "data": [
+//       {
+//         "day": "mon",
+//         "amount": 17.45
+//       },
+//       {
+//         "day": "tue",
+//         "amount": 34.91
+//       },
+//       {
+//         "day": "wed",
+//         "amount": 52.36
+//       },
+//       {
+//         "day": "thu",
+//         "amount": 31.07
+//       },
+//       {
+//         "day": "fri",
+//         "amount": 23.39
+//       },
+//       {
+//         "day": "sat",
+//         "amount": 43.28
+//       },
+//       {
+//         "day": "sun",
+//         "amount": 25.48
+//       }
+//     ]
+//   };
 
-generateGraph(Object.keys(data).length);
-sizeGraph(data);
+
+
+generateGraph(Object.keys(data["data"]).length);
+sizeGraph(data["data"]);
+setMetaData(data["meta"]);
