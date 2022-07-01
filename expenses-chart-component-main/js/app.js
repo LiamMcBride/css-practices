615ae4b30da9114ab2c01ceaa160a94bf5578442
LiamMcBride <mailmcbride56@gmail.com>
2022-07-01 14:41:46 -0400

    Chart is working properly. Dynmaically generates off of data.

---
+++ expenses-chart-component-main/js/app.js
@@ -0,0 +1,48 @@
+function calculateHeight(amount){
+    return (amount / 100) * 200;
+}
+
+function sizeGraph(data){
+    let graphs = document.getElementsByClassName("graph-bar");
+    let titles = document.getElementsByClassName("graph-label");
+    let counter = 0;
+    for(const day in data){
+        console.log(data[day])
+        graphs[counter].style.height = calculateHeight(data[day]["amount"]) + "px";
+        titles[counter].innerHTML = data[day]["day"]
+        counter++;
+    }
+}
+
+data = [
+    {
+      "day": "mon",
+      "amount": 17.45
+    },
+    {
+      "day": "tue",
+      "amount": 34.91
+    },
+    {
+      "day": "wed",
+      "amount": 52.36
+    },
+    {
+      "day": "thu",
+      "amount": 31.07
+    },
+    {
+      "day": "fri",
+      "amount": 23.39
+    },
+    {
+      "day": "sat",
+      "amount": 43.28
+    },
+    {
+      "day": "sun",
+      "amount": 25.48
+    }
+  ];
+
+sizeGraph(data);
